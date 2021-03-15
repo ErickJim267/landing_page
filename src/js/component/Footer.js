50a7318985348f51212f0c0d00cd514e49bbddd2
Rainfire267 <ejimenez267@gmail.com>
2021-03-15 18:40:16 +0000

    version 2

---
+++ src/js/component/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const MiFooter = () => {
+	return (
+		<div
+			className="container-fluid bg-dark text-center"
+			style={{ color: "white" }}>
+			<p>
+				<br />
+				<h3>Copyright © Your Website, 2021</h3>
+			</p>
+			<br />
+		</div>
+	);
+};
---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const MiJumbotron = () => {
+	return (
+		<div className="container">
+			<div className="jumbotron">
+				<h1 className="display-4">A warm welcome</h1>
+				<p className="lead">lorem impsum and a lot of dummy textS</p>
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Call to action!
+				</a>
+			</div>
+		</div>
+	);
+};
